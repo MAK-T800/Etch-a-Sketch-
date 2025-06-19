1bfe6e420a03eefc9eb26c242291a05b2c72df33
MAK-T800 <makknight6@gmail.com>
2025-06-19 13:46:09 +0530

    Add the Resise function in JS

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*This script creates a grid of squares that change color on hover.
+*/
 const container = document.getElementById('container');
 const gridSize = 16;
 const squareSize = 960 / gridSize;
@@ -16,3 +18,30 @@ function createGrid(size) {
 createGrid(gridSize);
 
 /*Hover effect for grid squares*/
+function addHoverEffect() {
+  const squares = document.querySelectorAll('.grid-square');
+  squares.forEach(square => {
+    square.addEventListener('mouseenter', () => {
+      square.style.backgroundColor = 'black';
+    });
+  });
+}
+
+createGrid(gridSize);
+addHoverEffect();
+
+/*Resize*/
+const buttonHtml = `<button id="resizeBtn">Resize Grid</button>`;
+document.body.insertAdjacentHTML('afterbegin', buttonHtml);
+
+const resizeBtn = document.getElementById('resizeBtn');
+resizeBtn.addEventListener('click', () => {
+  let newSize = prompt('Enter new grid size (max 100):');
+  newSize = Number(newSize);
+  if (newSize && newSize > 0 && newSize <= 100) {
+    createGrid(newSize);
+    addHoverEffect();
+  } else {
+    alert('Invalid size. Please enter a number between 1 and 100.');
+  }
+});
